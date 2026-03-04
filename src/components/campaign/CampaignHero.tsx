723e04c64b9e7a1ac504f20ff9439b6dda8dca88
Davidkirit <babaloladavid27@gmail.com>
2026-03-04 15:37:54 +0100

    fix: logo and added images

--- src/components/campaign/CampaignHero.tsx
+++ src/components/campaign/CampaignHero.tsx
@@ -21,9 +21,8 @@ export default function CampaignHero({ image }: CampaignHeroProps) {
         />
         <div className="absolute inset-0 bg-black/10 flex flex-col items-center justify-center text-center px-4">
           <div className="max-w-4xl space-y-6">
-            <h1 className="text-white text-5xl md:text-[96px] font-normal tracking-[-0.05em] leading-[1.22] font-tt lowercase">
-              every look is a <br />
-              <span className="text-[#C72f32] font-medium">statement</span>
+            <h1 className="text-white text-3xl md:text-[96px] font-normal tracking-[-0.05em] leading-[1.22] font-tt lowercase flex flex-wrap justify-center gap-x-2">
+              every look is a <span className="text-[#C72f32] font-medium">statement</span>
             </h1>
             <div className="flex justify-center pt-8">
               <img 
