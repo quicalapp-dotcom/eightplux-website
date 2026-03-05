abddf9dee76f7a96b94ef1319969cd9268dbf1d2
Abduljabar Asaju <zagajabar1@gmail.com>
2026-03-05 15:23:42 +0100

    changing of campain to lookbook

--- src/components/campaign/CampaignBanner.tsx
+++ src/components/campaign/CampaignBanner.tsx
@@ -7,14 +7,9 @@ import Link from 'next/link';
 interface CampaignBannerProps {
   image: string;
   showButtons?: boolean;
-  buttonText?: string;
-  buttonHref?: string;
 }
 
-export default function CampaignBanner({ image, showButtons,
-  buttonText = "EXPLORE", 
-  buttonHref = "/shop" 
- }: CampaignBannerProps) {
+export default function CampaignBanner({ image, showButtons }: CampaignBannerProps) {
   return (
     <section className="relative w-full overflow-hidden bg-black flex items-center justify-center">
       <div className="relative w-full">
@@ -27,19 +22,25 @@ export default function CampaignBanner({ image, showButtons,
           priority
         />
         <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center px-6">
-          <h1 className="text-white text-5xl md:text-[96px] font-normal mb-16 tracking-[-0.05em] leading-[1.22] font-tt lowercase">
+          <h1 className="text-white text-5xl md:text-[96px] font-normal tracking-[-0.05em] leading-[1.22] font-tt lowercase">
             dress easy live <span className="text-[#C72f32] font-medium">bold</span>
           </h1>
         
         {showButtons && (
-          <div className="flex justify-center w-full max-w-xs sm:max-w-none mx-auto px-4">
-          <Link 
-            href={buttonHref}
-            className="inline-block border border-white/50 text-white px-16 py-4 text-[10px] sm:text-[11px] font-bold uppercase tracking-[0.5em] hover:bg-white hover:text-black transition-all duration-300 backdrop-blur-sm w-full sm:w-auto text-center"
-          >
-            {buttonText}
-          </Link>
-        </div>
+          <div className="flex flex-col sm:flex-row gap-4 w-full max-w-lg mx-auto px-4">
+            <Link 
+              href="/shop"
+              className="bg-white text-black py-4 px-12 text-sm font-medium  tracking-tight hover:bg-black hover:text-white transition-all duration-300 text-center w-full"
+            >
+              Shop XX
+            </Link>
+            <Link 
+              href="/shop"
+              className="bg-white text-black py-4 px-12 text-sm font-medium tracking-tight hover:bg-black hover:text-white transition-all duration-300 text-center w-full"
+            >
+              Shop XY
+            </Link>
+          </div>
         )}
       </div>
       </div>
